c92af1aa20d6a2a55771909b304d9c64c84aa391
tamnalee <105832393+tamnalee@users.noreply.github.com>
2022-05-23 16:47:52 +0900

    add

---
+++ app.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+from app_eda import run_eda
+from app_home import run_home
+
+def main():
+    st.title('자동차 가격 예측')
+
+    menu = ['Home', 'EDA', 'ML']
+    choice = st.sidebar.selectbox('메뉴선택',menu)
+
+    if choice == menu[0] :
+        run_home()
+    elif choice == menu[1] :
+        run_eda()
+    elif choice == menu[2] :
+        pass
+
+    st.balloons()
+
+
+if __name__ == '__main__' :
+    main()
---
+++ import joblib.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import joblib
+import numpy as np
+import sklearn
+
+
+
+def run_mi() :
+    st.subheader('자동차 구매가능 금액 예측')
+
+    print(sklearn.__version__)
+
+    # 예측하기 위해서 필요한 파일들을 불러와야 된다.
+    # 이 예에서는, 인공지는파일, x스케일러 파일, y스케일러파일
+    # 3개를 불러와야한다.
+
+    regressor = joblib.load('data/regressor.pkl')
+    scaler_X = joblib.load('data/scaler_X.pkl')
+    scaler_y = joblib.load('data/scaler_y.pkl')
+
+    # 성별, 나이, 연봉, 카드빛, 자산을 입력받도록 만드세요.
+
+    gender = st.radio('성별입력', ['남자','여자'])
+ㄴ
+    if gender == '여자' :
+        gender = 0
+    else :
+        gender = 1
+
+    age = st.number_input('나이입력', 0, 120)
+    salary = st.number_input('연봉입력', 0)
+    dept = st.number_input('카드빛 입력', 0)
+    worth = st.number_input('자산입력', 0)
+
+    if st.st.button('자동차 구매 금액 예측') :
+
+   
+    # 1. 신규 고객의 정보를 넘파이 어레이로 만들어준다.
+    new_data = np.array([gender, age, salary, debt, worth])
+
+
+    # 2. 학습할 때 사용한 X의 피처 스케일러를 이용해서 피쳐스케일링한다.
+    # 먼저, 데이터를 2차원으로 만들어준다.
+    new_data = new_data.reshape(1,5)
+    new_data = scaler.transform(new_data)
+
+    # 3. 인공지능에게 예측해달라고 한다.
+    y_pred = regressor.predict(new_data)
+
+    # 4. 예측한 값을, 원상복구 시킨다.
+    y_pred = scaler_y.inverse_transform(y_pred)
+
+    y_pred = round(y_pred[0],[0])
+    st.write('이 사람의 구매 가능 금액은' + str (y_pred) +'달러입니다.') 
+
+    
+    
+    
+    
